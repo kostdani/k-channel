03f9f3ae635e1b471c1ace2ace9c288956861fdd
kostdani <kostdani@fit.cvut.cz>
2023-04-05 17:03:52 +0200

    Update emacs.scm

--- k-channel/packages/emacs.scm
+++ k-channel/packages/emacs.scm
@@ -19,7 +19,7 @@
 	     ))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1r2hxmbkdf6pxvj6piwdiz4f9amlkrk64846klvy4ri3xgxnp33y"))))
+        (base32 "0mi64a6pnblfqx278isbx487xhijs1q1cgjpswsvmi7qza2v8viq"))))
     (build-system emacs-build-system)
     (propagated-inputs
      (list emacs-dash emacs-editorconfig emacs-s node))
