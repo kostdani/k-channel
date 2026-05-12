23a451dd3848ed0b1ec5ee2fb40002acb909df6b
kostdani <kostdani@fit.cvut.cz>
2026-05-13 00:43:43 +0200

    Fix version string formatting in emacs-copilot

--- k-channel/packages/emacs-xyz.scm
+++ k-channel/packages/emacs-xyz.scm
@@ -12,13 +12,13 @@
 (define-public emacs-copilot
   (package
     (name "emacs-copilot")
-    (version "v0.5.0")
+    (version "0.5.0")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/copilot-emacs/copilot.el")
-             (commit version)))
+             (commit (string-append "v" version))))
        (sha256
         (base32
          "1xvhfwgddms0cxhi9pn75vb6qsd6gqfv8s59xjk9ilh57nvwzqfn"))
